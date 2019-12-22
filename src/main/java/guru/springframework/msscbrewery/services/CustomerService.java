d7128f349328a9c23608ce8d2204cf1577913071
Onno Schmidt <onno@impliz.it>
2019-12-22 23:39:54 +0100

    Handle customer crud

--- src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -9,4 +9,10 @@ import java.util.UUID;
  */
 public interface CustomerService {
     CustomerDto getCustomerById(UUID customerId);
+
+    CustomerDto createNewBeer(CustomerDto customerDto);
+
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+
+    void deleteCustomerById(UUID customerId);
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -8,13 +9,33 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-04-21.
  */
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
         return CustomerDto.builder()
                 .id(UUID.randomUUID())
                 .name("Joe Buck")
                 .build();
+    }
+
+    @Override
+    public CustomerDto createNewBeer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name(customerDto.getName())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // todo implement
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+        log.debug("Delete customer " + customerId);
     }
 }
